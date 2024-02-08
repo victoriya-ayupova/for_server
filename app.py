dff5ad045dc357621699e1706de15da09e7b9603
Виктория Аюпова <vicktoria.ayupova@yandex.ru>
2024-02-08 14:49:53 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.get('/')
+def index():
+    return render_template('index.html')
+
+
+@app.post('/')
+def index_post():
+    form = request.form
+    name = form['name']
+    return render_template('greetings.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run()
